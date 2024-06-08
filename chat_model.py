b36a84d558f02465a61e8a09052c7ccbd0ced0cc
Soham-Rao <sohamsrao21@gmail.com>
2024-06-08 16:04:35 +0530

    updated unused code

--- chat_model.py
+++ chat_model.py
@@ -1,8 +1,5 @@
 import nltk
-from pymongo import MongoClient
 import json
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 import pickle
 import random
@@ -12,84 +9,54 @@ from keras.src.layers.core.dense import Dense
 from keras.src.layers.regularization.dropout import Dropout
 from keras.src.optimizers.sgd import SGD
 
-# Initialize lemmatizer
 lemmatizer = WordNetLemmatizer()
 
-# Replace with your MongoDB connection string
-connection_string = "mongodb://localhost:27017/"
-
-# Create a MongoClient to the running MongoDB instance
-client = MongoClient(connection_string)
-
-# Access the 'articles' database
-db = client['articles']
-
-# Access the 'articles' collection
-collection = db['articles']
-
-# Initialize lists
 words = []
 classes = []
 documents = []
 ignore_words = ['?', '!']
 
-# Load intents file
 data_file = open('intents.json').read()
 intents = json.loads(data_file)
 
 # Tokenize and preprocess data from intents
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-        # Tokenize each word
         w = nltk.word_tokenize(pattern)
         words.extend(w)
-        # Add documents in the corpus
         documents.append((w, intent['tag']))
-        # Add to classes list
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-# Lemmatize, lower each word, and remove duplicates
+# Lemmatize and remove duplicates
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
-
-# Sort classes
 classes = sorted(list(set(classes)))
 
-# Print information
 print(len(documents), "documents")
 print(len(classes), "classes", classes)
 print(len(words), "unique lemmatized words", words)
 
-# Save words and classes
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
 
-# Create training data
 training = []
 output_empty = [0] * len(classes)
 
 # Training set, bag of words for each sentence
 for doc in documents:
-    # Initialize our bag of words
     bag = []
-    # List of tokenized words for the pattern
     pattern_words = doc[0]
-    # Lemmatize each word - create base word, in attempt to represent related words
     pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
     
-    # Create our bag of words array with 1, if word match found in current pattern
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
     
-    # Output is a '0' for each tag and '1' for current tag (for each pattern)
     output_row = list(output_empty)
     output_row[classes.index(doc[1])] = 1
 
-    # Add the bag of words and the output row to the training set
     training.append([bag, output_row])
 
-# Shuffle our features and turn into np.array
 random.shuffle(training)
 training = np.array(training, dtype=object)
 
@@ -99,8 +66,7 @@ train_y = np.array(list(training[:, 1]), dtype=np.float32)
 
 print("Training data created")
 
-# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
-# equal to number of intents to predict output intent with softmax
+# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons equal to number of intents to predict output intent with softmax
 model = Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
@@ -108,11 +74,10 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+# Compile model. Stochastic gradient descent with Nesterov accelerated gradient
 sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-# Fitting and saving the model 
 hist = model.fit(train_x, train_y, epochs=200, batch_size=5, verbose=1)
 model.save('chatbot_model.h5', hist)
 
